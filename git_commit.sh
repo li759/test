a034b6cb71ed9fc7d15cd02dfadb5e4cf7c3765a
Qilin <1512499938@qq.com>
2025-09-10 11:19:12 +0800

    Auto commit on closing Obsidian - 2025-09-10 11:19:12

--- git_commit.sh
+++ git_commit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 设置你的 Obsidian 笔记库路径
-VAULT_PATH="/home/qilin/文档/qilin"
+VAULT_PATH="/home/qilin/文档/rl/test"
 cd "$VAULT_PATH"
 # 获取当前日期和时间
 current_date=$(date +"%Y-%m-%d %H:%M:%S")
--- sac_training/math.md
+++
@@ -1,3 +0,0 @@
-[[sac_training]]
-
----					
